29c8fb37206da2c020c262e3fc0232268f61642f
mohammedtahairp-wq <mohammedtahairp@gmail.com>
2025-08-18 13:51:11 +0530

    title change

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind CSS v4 Setup</title>
+    <title>Oreo Technologies</title>
     <link href="./assets/css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
